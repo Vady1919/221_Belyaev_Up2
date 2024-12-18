4986e071e63d3f2e7f40508def9dcf97eb826227
Vady1919 <157110820+Vady1919@users.noreply.github.com>
2024-12-18 20:25:53 +0300

    Добавьте файлы проекта.

---
+++ 221_Belyaev_Up2/MaterialDeffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221_Belyaev_Up2
+{
+    internal class MaterialDeffect
+    {
+        public static int MatDef(int productId, int materialType, int quantity, double par1, double par2)
+        {
+            double result;
+            var TypeProduct = Belyaev_UP2Entities.GetContext().ProductsType.Where(x => x.ProductsTypeID == productId);
+            var typeMaterial = Belyaev_UP2Entities.GetContext().MaterialType.Where(x => x.materialType1 == materialType);
+            if (TypeProduct.Count() < 1 | typeMaterial.Count() < 1 | quantity < 1 | par1 <= 0 | par2 <= 0)
+                return -1;
+            else
+            {
+                double coeff = (double)TypeProduct.Select(x => x.Ratio).FirstOrDefault();
+                double materCount = quantity + quantity * typeMaterial.Select(x => x.DefectPercent).FirstOrDefault();
+                result = materCount * par1 * par2 * coeff;
+                return (int)Math.Ceiling(result);
+            }
+
+        }
+    }
+}
+
---
+++ 221_Belyaev_Up2/Pages1/AddPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _221_Belyaev_Up2.Pages1
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Partners _currentPartners = new Partners();
+        public AddPage(Partners selectedPartners)
+        {
+            InitializeComponent();
+            Type.ItemsSource = Belyaev_UP2Entities.GetContext().PartnersType.ToList();
+            if (selectedPartners != null)
+                _currentPartners = selectedPartners;
+
+            DataContext = _currentPartners;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (Rating.Text != "")
+            {
+                try
+                {
+                    _currentPartners.Rating = int.Parse(Rating.Text);
+                    if (int.Parse(Rating.Text) < 0) errors.AppendLine("Рейтинг не может быть отрицательным!");
+                }
+                catch (Exception)
+                {
+                    errors.AppendLine("Некорректное значение рейтинга!");
+                }
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            else
+                if (_currentPartners.PartnerID == 0)
+                Belyaev_UP2Entities.GetContext().Partners.Add(_currentPartners);
+            try
+            {
+                Belyaev_UP2Entities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно сохранены!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
